9919e114addfe77086ac376972a0ed0767a914f0
Amitkumarkoli <akdev523@gmail.com>
2024-01-07 13:20:33 +0530

    adding all new files

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(title: ''),
+    debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(widget.title),
+        leading: const Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/profile.jpeg'),
+              radius: 13,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text('Lisa',
+            style: TextStyle(
+              fontSize: 10,
+              fontWeight: FontWeight.bold
+            ),)
+          ],
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)),
+        ],
+      ),
+    );
+  }
+}
