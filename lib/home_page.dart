5c542be216983b67bf4af379b8a1ce0a60f2ccbc
Amitkumarkoli <akdev523@gmail.com>
2024-01-07 22:25:36 +0530

    update home page with image

---
+++ lib/home_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(widget.title),
+        leading: const Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/profile.jpeg'),
+              radius: 13,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text('Lisa',
+            style: TextStyle(
+              fontSize: 10,
+              fontWeight: FontWeight.bold
+            ),)
+          ],
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)),
+        ],
+      ),
+      body: Row(
+        
+        children: [
+          const Padding(padding: EdgeInsets.all(18)),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                child: Container(
+                  width: 350,
+                  height: 160,
+                    margin: const EdgeInsets.only(top:80 ),
+                  decoration:  BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    image: const DecorationImage(image: AssetImage('assets/house.jpeg'),
+                    fit: BoxFit.cover               
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20,),
+                const Text('Rosewood Retreat',
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.bold
+                ),)
+            ],         
+          ),
+        ],
+      ),
+    );
+  }
+} 
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:house_harbor/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,52 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(title: ''),
-    debugShowCheckedModeBanner: false,
+    return  const MaterialApp(
+    debugShowCheckedModeBanner : false,
+     home: HomePage(title: '',),
+    
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Text(widget.title),
-        leading: const Row(
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/profile.jpeg'),
-              radius: 13,
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Text('Lisa',
-            style: TextStyle(
-              fontSize: 10,
-              fontWeight: FontWeight.bold
-            ),)
-          ],
-        ),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)),
-        ],
-      ),
-    );
-  }
-}
+// this is main structure means root of our project
