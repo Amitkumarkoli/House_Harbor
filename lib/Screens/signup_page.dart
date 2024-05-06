d594e0799d25a1921b78db866c7f7e9c9145be47
Amitkumarkoli <akdev523@gmail.com>
2024-05-06 17:45:19 +0530

    update signup screen UI

--- lib/Screens/signup_page.dart
+++ lib/Screens/signup_page.dart
@@ -13,34 +13,46 @@ class _LoginPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Enter your email',
-                border:OutlineInputBorder()
-              )),
-            SizedBox(height: 20),
-            TextField( 
-              decoration: InputDecoration(
-              labelText: 'Create your password',
-              border: OutlineInputBorder(),
-              )),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+          image: AssetImage('assets/image/background_signup.png'),
+          fit: BoxFit.cover,
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Enter your email',
+                  border:OutlineInputBorder(),
+                  filled: true,
+                  fillColor: Colors.white
+                )),
               SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
-                }, 
-                style: ButtonStyle(
-                 minimumSize: MaterialStateProperty.all(
-                  Size(double.infinity, 50)),
-                 backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 105, 240, 174)),
-                ),
-                child: Text('SignUp'))
-          ],
+              TextField( 
+                decoration: InputDecoration(
+                labelText: 'Create your password',
+                border: OutlineInputBorder(),
+                filled: true,
+                fillColor: Colors.white
+                )),
+                SizedBox(height: 25),
+                ElevatedButton(
+                  onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                  }, 
+                  style: ButtonStyle(
+                   minimumSize: MaterialStateProperty.all(
+                    Size(double.infinity, 50)),
+                   backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 105, 240, 174)),
+                  ),
+                  child: Text('SignUp'))
+            ],
+          ),
         ),
       ),
     );
