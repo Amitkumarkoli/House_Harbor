cc876a80889a7a958e0719987eb91ee43721e035
Amitkumarkoli <akdev523@gmail.com>
2024-05-06 17:15:44 +0530

    update login screen UI

--- lib/Screens/login_page.dart
+++ lib/Screens/login_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:house_harbor/Screens/home_page.dart';
 import 'package:house_harbor/Screens/signup_page.dart';
 
-
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -14,42 +13,62 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        body: Container(
+          decoration: BoxDecoration(
+          image: DecorationImage(
+          image: AssetImage("assets/image/background_image.png"),
+          fit: BoxFit.cover,
+          ),),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
               decoration: InputDecoration(
-                labelText: 'Email',
-                border:OutlineInputBorder()
-              )),
+              labelText: 'Email',
+              border: OutlineInputBorder(),
+              filled: true,
+              fillColor: Colors.white,
+            )),
             SizedBox(height: 20),
-            TextField( 
+            TextField(
               decoration: InputDecoration(
               labelText: 'Password',
               border: OutlineInputBorder(),
-              )),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
-                }, 
+              filled: true,
+              fillColor: Colors.white,
+            )),
+            SizedBox(height: 25),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => HomePage()));
+                },
                 style: ButtonStyle(
-                 minimumSize: MaterialStateProperty.all(
-                  Size(double.infinity, 50)),
-                 backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 105, 240, 174)),
+                  minimumSize: MaterialStateProperty.all(Size(double.infinity, 50)),
+                  backgroundColor: MaterialStateProperty.all<Color>( Color.fromARGB(255, 105, 240, 174)),
                 ),
                 child: Text('Login')),
-                SizedBox(height: 10),
-                TextButton(
-                  onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
-                  }, 
-                  child: Text("don't have a account? SignUp"))
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Don't have an account?",
+                    style: TextStyle(
+                    color: Colors.white,
+                    decoration: TextDecoration.underline)),
+                SizedBox(width: 5),
+                TextButton( 
+                  onPressed: () {
+                    Navigator.push( context, MaterialPageRoute(builder: (context) => SignupPage()));
+                  },
+                child: Text("SignUp")),
+              ],
+            )
           ],
         ),
       ),
-    );
+    ));
   }
 }
